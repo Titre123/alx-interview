ce18ced05d192d0ab612bbb4f106825553870816
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2023-01-09 03:16:15 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,7 +2,7 @@
 """
 This is unlock boxes
 """
-    def canUnlockAll(boxes):
+def canUnlockAll(boxes):
     '''Checks if all the boxes in a list of boxes containing the keys
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
