7ec1aa327999b4d4a0b8d5691139c04dc8eb2aaa
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2023-01-13 02:33:52 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+write a method that calculates the fewest number of
+operations needed to result in exactly n H characters in the file.
+'''
+def minOperations(n):
+  '''
+  Args: n
+  '''
+    if n == 0:
+        return 0
+    operations = 0
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            operations += i
+            n /= i
+        i += 1
+    if n > 1:
+        operations += n
+    return operations
