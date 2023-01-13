7c6b14f88fb369fdcccf01de5bd80df36c5dfbab
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2023-01-13 21:45:57 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,18 +6,24 @@ operations needed to result in exactly n H characters in the file.
 
 
 def minOperations(n):
-  '''
-  Args: n
-  '''
-    if n == 0:
+    '''
+    Args: n
+    '''
+    operation = 0
+    if n == 1 or not isinstance(n, int):
         return 0
-    operations = 0
-    i = 2
-    while i * i <= n:
-        while n % i == 0:
-            operations += i
-            n /= i
-        i += 1
-    if n > 1:
-        operations += n
-    return operations
+    while n > 1:
+        if(n % 2 == 0):
+            operation += 2
+            n = n / 2
+        elif (n % 3 == 0):
+            operation += 3
+            n = n / 3
+        elif (n % 5 == 0):
+            operation += 5
+            n = n / 5
+        elif (n % 7 == 0):
+            operation += 7
+            n = n / 7
+    return operation
+  
