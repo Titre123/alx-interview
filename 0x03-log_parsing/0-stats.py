c81b727f3da55d8576bf62e3bfaf6992f8f11731
Taiwo <taiwotriumphant@gmail.com>
2023-01-24 08:08:30 -0800

    testing stdin

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,6 +7,8 @@ import sys
 number = 0
 status_code = [200, 301, 400, 401, 403, 404, 405, 500]
 size = 0
+length = len(sys.stdin.readlines())
+print(length)
 status_count = {}
 for line in sys.stdin:
     number += 1
@@ -20,9 +22,10 @@ for line in sys.stdin:
             status_count[status] += 1
         else:
             status_count[status] = 1
+    print(status_count)
 
     try:
-        if number % 10 == 0:
+        if number % 10 == 0 or number == length:
             print('File size: {}'.format(size))
             for status in status_code:
                 if status in status_count:
