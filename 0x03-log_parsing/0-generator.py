138be344d6c16a86993fe785e39163e665cace56
Taiwo <taiwotriumphant@gmail.com>
2023-01-25 01:07:58 +0100

    new

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -4,13 +4,7 @@ import sys
 from time import sleep
 import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(
-            1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+with open('./xt.txt', 'r') as f:
+    fs = f.readlines()
+    for line in fs:
+        sys.stdout.write(line)
