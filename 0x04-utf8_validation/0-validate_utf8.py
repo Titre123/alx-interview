ffb6ba2ff8be87632d5280632551309dbd0fac9c
Taiwo <taiwotriumphant@gmail.com>
2023-02-03 00:41:21 +0100

    n queens

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,4 +7,23 @@ def validUTF8(data):
         Args:
             - data
     """
-    return True if sorted(data)[len(data) - 1] < 128 else False
+    binary = ''
+    for item in data:
+        bina = str(bin(item))
+        new_str = bina.replace('0b', '')
+        
+        binary += f' {new_str}'
+
+    return binary
+
+
+# if len(new_str) < 8:
+#             left = 8 - len(new_str)
+#             zero_added = '0' * left
+#             new = zero_added + new_str
+#             binary += f' {new}'
+#         elif len(new_str) > 8:
+#             left = len(new_str) - 8
+#             new = new_str[left:]
+#             binary += f' {new}'
+#         else:
