410fab37f3128d2f3c9dc8e2765b9cdd47dcf06c
Taiwo <taiwotriumphant@gmail.com>
2023-02-23 23:21:10 +0100

    done with this

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,7 +2,6 @@
 '''
     Given a pile of coins of different values, determine the
     fewest number of coins needed to meet a given amount total.
-
     Prototype: def makeChange(coins, total)
     Return: fewest number of coins needed to meet total
     If total is 0 or less, return 0
